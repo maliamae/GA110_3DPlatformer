95798bafc53077b3a927be6e3767294b6a836d6b
maliamae <maliakephart@lcad.edu>
2026-02-05 20:23:12 -0800

    player controller basic set up

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,116 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public CinemachineCamera playerCam;
+
+    private Vector2 cameraRotation = Vector2.zero;
+    private Vector2 playerTargetRotation = Vector2.zero;
+
+    public float lookSenseH = 0.1f;
+    public float lookSenseV = 0.1f;
+    public float lookLimitV = 89f;
+
+    public float gravity = -9.81f;
+    public float jumpForce = 5f;
+
+    private Vector2 move;
+    private Vector3 targetDir;
+    private Vector2 look;
+    private Vector3 velocity;
+    private bool isDashing;
+    private bool isGrounded;
+    private bool isDead = false;
+
+    public float speed;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private bool cursorOn = true;
+
+    private void Update()
+    {
+        Move();
+
+        if (Input.GetMouseButtonDown(0) && cursorOn)
+        {
+            DisableCursor();
+        }
+
+        if (!controller.isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+        //Debug.Log(controller.isGrounded);
+    }
+
+    private void LateUpdate()
+    {
+        cameraRotation.x += lookSenseH * look.x;
+        cameraRotation.y = Mathf.Clamp(cameraRotation.y - lookSenseV * look.y, -lookLimitV, lookLimitV);
+
+        playerTargetRotation.x += transform.eulerAngles.x + lookSenseH * look.x;
+        transform.rotation = Quaternion.Euler(0f, playerTargetRotation.x, 0f);
+
+        playerCam.transform.rotation = Quaternion.Euler(cameraRotation.y, cameraRotation.x, 0f);
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        move = inputValue.Get<Vector2>();
+        
+    }
+
+    private void OnJump(InputValue inputValue)
+    {
+        Jump();
+    }
+
+    private void OnDash(InputValue inputValue)
+    {
+
+    }
+
+    private void OnLook(InputValue inputValue)
+    {
+        look = inputValue.Get<Vector2>();
+    }
+
+    private void Move()
+    {
+        targetDir = new Vector3(move.x, 0f, move.y).normalized;
+
+        if (targetDir.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(targetDir.x, targetDir.z) * Mathf.Rad2Deg + playerCam.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        
+    }
+
+    private void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            //velocity.y += gravity * Time.deltaTime;
+            velocity.y = jumpForce;
+            
+        }
+    }
+
+
+    private void DisableCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        //Debug.Log("click");
+    }
+}
